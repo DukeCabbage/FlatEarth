c634c613bd008f41003e7b6dee5cdc598e80669c
Liao Wang <leo@Liaos-MacBook-Pro.local>
2018-10-22 16:36:52 -0700

    Add basic ar core dependencies

--- app/src/main/java/com/cabbage/flatearth/MainActivity.kt
+++ app/src/main/java/com/cabbage/flatearth/MainActivity.kt
@@ -1,99 +1,44 @@
 package com.cabbage.flatearth
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.hardware.Camera
 import android.os.Bundle
+import android.view.Gravity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.appcompat.app.AppCompatActivity
-import android.view.View
-import android.widget.FrameLayout
-import android.widget.ImageButton
 import android.widget.Toast
+import com.cabbage.flatearth.misc.checkIsSupportedDeviceOrFinish
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
 
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-    private var mCamera: Camera? = null
-    private var mCameraView: CameraView? = null
+    private val arFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment
+    }
+
+    private var andyRenderable: ModelRenderable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (!checkIsSupportedDeviceOrFinish()) return
         setContentView(R.layout.activity_main)
 
-        //btn to close the application
-        val imgClose = findViewById<View>(R.id.imgClose) as ImageButton
-        imgClose.setOnClickListener { System.exit(0) }
-    }
+        val lightEnabled = arFragment.arSceneView.isLightEstimationEnabled
+        Timber.i("$lightEnabled")
 
-    public override fun onStart() {
-        super.onStart()
-        initCamera()
-    }
-
-    public override fun onStop() {
-        super.onStop()
-        releaseCameraAndPreview()
-    }
-
-
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
-        when (requestCode) {
-            PERMISSION_REQUEST_CAMERA -> {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    initCamera()
-                } else {
-                    Toast.makeText(this, "Camera permission NOT granted", Toast.LENGTH_SHORT).show()
+        // When you build a Renderable, Sceneform loads its resources in the background while returning
+        // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
+        ModelRenderable.builder()
+                .setSource(this, R.raw.andy)
+                .build()
+                .thenAccept { andyRenderable = it }
+                .exceptionally { _ ->
+                    val toast = Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG)
+                    toast.setGravity(Gravity.CENTER, 0, 0)
+                    toast.show()
+                    null
                 }
-                return
-            }
-        }
-    }
-
-    private fun initCamera() {
-
-        val permissionCheck = ContextCompat.checkSelfPermission(this,
-                                                                Manifest.permission.CAMERA)
-
-        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show()
-        } else {
-            ActivityCompat.requestPermissions(this,
-                                              arrayOf(Manifest.permission.CAMERA),
-                                              PERMISSION_REQUEST_CAMERA)
-
-            return
-        }
-
-        try {
-            releaseCameraAndPreview()
-            mCamera = Camera.open()//you can use open(int) to use different cameras
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Timber.e("Failed to get camera: %s", e.message)
-        }
-
-        if (mCamera != null) {
-            mCameraView = CameraView(this, mCamera)//create a SurfaceView to show camera data
-            val camera_view = findViewById<View>(R.id.camera_view) as FrameLayout
-            camera_view.addView(mCameraView)//add the SurfaceView to the layout
-        }
-    }
-
-    private fun releaseCameraAndPreview() {
-        if (mCamera != null) {
-            mCamera!!.release()
-            mCamera = null
-        }
-    }
-
-    companion object {
-
-        private val MAIN_ACTIVITY_CODE = 1
-        private val PERMISSION_REQUEST_CAMERA = MAIN_ACTIVITY_CODE * 100 + 1
     }
 }
