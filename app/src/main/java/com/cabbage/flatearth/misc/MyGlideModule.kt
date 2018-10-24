f21b7093b99ff764651d7e1b7a2d6103989ab040
liaowng <liaowng@gmail.com>
2018-10-24 10:43:36 -0700

    Add glide

---
+++ app/src/main/java/com/cabbage/flatearth/misc/MyGlideModule.kt
@@ -0,0 +1,7 @@
+package com.cabbage.flatearth.misc
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class MyAppGlideModule : AppGlideModule()
---
+++ app/src/main/java/com/cabbage/flatearth/mock/Mock.kt
@@ -0,0 +1,5 @@
+package com.cabbage.flatearth.mock
+
+const val testUrl = "https://firebasestorage.googleapis.com/v0/b/" +
+                    "project-nephele.appspot.com/o/terran_vic_full.jpg" +
+                    "?alt=media&token=07c59def-f08a-4596-95a6-d03615d5c0c2"
